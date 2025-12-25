d04d1e93376c6b374ad61642fa1e2a8c72d7cd95
HunterSolarious <bosskuradam04@gmail.com>
2025-12-25 14:32:30 +0800

    Remove sudo from install.sh commands

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install -y docker.io
+ apt-get install -y docker.io
 
-sudo apt-get install -y curl
+ apt-get install -y curl
 
-sudo apt-get install -y npm
+ apt-get install -y npm
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+ curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
 
 git clone https://github.com/IoTKETI/Mobius.git
 
@@ -18,10 +18,11 @@ sudo npm install
 
 cd ..
 
-sudo docker-compose up --no-start
+ docker compose up --no-start
 
 echo -e "\n===========Result of Mobius docker install==============\n"
-sudo docker-compose ps
+echo -e "\n===========script by yadangsol==============\n"
+ docker compose ps
 echo -e "\n========================================================\n"
 
 
